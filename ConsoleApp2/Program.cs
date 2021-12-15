5bf1d76db62829c249ff893cb98259fa1d3044e6
Alexander <polyak.98@list.ru>
2021-12-15 10:33:25 +0300

    Empty Main

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,21 +6,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            int number = 11;
-            int counter = 0;
-            int ost;
-            int[] array_of_numbers = new int[100];
-            while (number > 0)
-            {
-                ost = number % 2;
-                number /= 2;
-                array_of_numbers[counter] = ost;
-                counter += 1;
-            }
-            for (int i = 0; i < array_of_numbers.Length; i++)
-            {
-                Console.Write(array_of_numbers[i]);
-            }
+            
         }
     }
 }
